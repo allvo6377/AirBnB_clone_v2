001639bcff6db5e16034b7ca3ffb48d984db58a8
allvo6377 <alvinpilot@gmail.com>
2023-07-07 02:30:39 +0300

    result

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-from fabric.api import local
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
-import os
+from fabric.api import local
 
 
 def do_pack():
-    """Create a .tgz archive from the contents of the web_static folder."""
-    # Create the versions folder if it doesn't exist
-    if not os.path.exists('versions'):
-        os.makedirs('versions')
-
-    # Generate the archive path
-    now = datetime.now()
-    archive_name = 'web_static_{}{}{}{}{}{}.tgz'.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
-    archive_path = os.path.join('versions', archive_name)
-
-    # Create the archive using tar and gzip
-    try:
-        local('tar -czvf {} web_static'.format(archive_path))
-        return archive_path
-    except Exception as e:
-        print('An error occurred during archive creation:', e)
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-archive_path = do_pack()
-if archive_path is not None:
-    print('Archive created:', archive_path)
-else:
-    print('Failed to create archive.')
-
+    return file
