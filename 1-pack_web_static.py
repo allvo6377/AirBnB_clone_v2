d3bf967cf77ec5d0529df23643e6c181ffc4b24a
allvo6377 <alvinpilot@gmail.com>
2023-07-07 02:00:27 +0300

    result

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,36 +1,17 @@
-#!/usr/bin/python3
-'''Fabric script to generate .tgz archive'''
+#!/usr/bin/bash
+"""Generates a Compress (tgz) archive"""
+
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
-    Returns the archive path if successful, otherwise returns None.
-    """
-    try:
-        # Create the versions folder if it doesn't exist
-        local("mkdir -p versions")
-
-        # Generate the archive path
-        now = datetime.now()
-        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
-            now.year, now.month, now.day, now.hour, now.minute, now.second)
-        archive_path = "versions/{}".format(archive_name)
-
-        # Compress the web_static folder into the archive
-        local("tar -czvf {} web_static".format(archive_path))
-
-        # Return the archive path if successful
-        return archive_path
-    except Exception as e:
-        print("An error occurred during archive generation: {}".format(e))
+    """Generate a .tgz archive from the contents of the web_static folder."""
+    local("mkdir -p versions")
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(time)
+    result = local("tar -cvzf {} web_static".format(file_name))
+    if result.failed:
         return None
+    return file_name
 
-# Calling the do_pack function to generate the archive
-archive_path = do_pack()
-if archive_path:
-    print("Archive generated: {}".format(archive_path))
-else:
-    print("Archive generation failed.")
