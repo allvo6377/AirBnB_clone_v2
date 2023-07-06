abc6de5532ccf96802cdc99bb397d2da6e8ff416
allvo6377 <alvinpilot@gmail.com>
2023-07-06 23:25:30 +0000

    new

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,30 @@
 #!/usr/bin/bash
-"""Generates a Compress (tgz) archive"""
-
 from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
-    """Generate a .tgz archive from the contents of the web_static folder."""
-    local("mkdir -p versions")
-    time = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(time)
-    result = local("tar -cvzf {} web_static".format(file_name))
-    if result.failed:
+    """Create a .tgz archive from the contents of the web_static folder."""
+    # Create the versions folder if it doesn't exist
+    if not os.path.exists('versions'):
+        os.makedirs('versions')
+
+    # Generate the archive path
+    now = datetime.now()
+    archive_name = 'web_static_{}{}{}{}{}{}.tgz'.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
+    archive_path = os.path.join('versions', archive_name)
+
+    # Create the archive using tar and gzip
+    try:
+        local('tar -czvf {} web_static'.format(archive_path))
+        return archive_path
+    except Exception as e:
+        print('An error occurred during archive creation:', e)
         return None
-    return file_name
+archive_path = do_pack()
+if archive_path is not None:
+    print('Archive created:', archive_path)
+else:
+    print('Failed to create archive.')
 
