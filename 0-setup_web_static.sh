4ee932aed706d9c891109658679dea35ba68faa4
allvo6377 <alvinpilot@gmail.com>
2023-07-09 04:46:07 +0000

    test-worked

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,7 +16,7 @@ echo "<html>
   <body>
     <h1>Test</h1>
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -24,8 +24,9 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Update Nginx configuration to serve hbnb_static
+sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
+
