1629d9f2c8aad4c0def9458e7e6f0032a16b2462
allvo6377 <alvinpilot@gmail.com>
2023-07-09 04:00:25 +0000

    deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
-# Bash script that sets up the web servers for the deployment of web_static
-apt-get -y update
-apt-get -y upgrade
+# Set up web servers for the deployment of web_static
 
-# Installs Nginx
-apt-get -y install nginx
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
 
-# Creates the folders for the deployment of web_static
-mkdir -p /data/web_static/releases/test /data/web_static/shared
+# Create directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
-# Creates a fake HTML file
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Alx School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    <h1>Test</h1>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Creates a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Gives ownership of the /data/ folder to the ubuntu user and group
-chown -hR ubuntu:ubuntu /data/
+# Give ownership to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
-# Updating the Nginx configuration to serve the content of /data/web_static/current/
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Update Nginx configuration
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-# Restarting Nginx after applying changes
-service nginx start
+# Restart Nginx
+sudo service nginx restart
